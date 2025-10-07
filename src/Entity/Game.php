3dbe4d4fc5cfb52a86482a4d43c6c50fbbf972d0
marcusWeb04 <marcusfavernay@gmail.com>
2025-10-07 21:07:38 +0200

    création de plusieurs entitées

--- src/Entity/Game.php
+++ src/Entity/Game.php
@@ -3,6 +3,8 @@
 namespace App\Entity;
 
 use App\Repository\GameRepository;
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
 
 #[ORM\Entity(repositoryClass: GameRepository::class)]
@@ -13,8 +15,89 @@ class Game
     #[ORM\Column]
     private ?int $id = null;
 
+    #[ORM\Column(length: 255)]
+    private ?string $name = null;
+
+    /**
+     * @var Collection<int, TypeGame>
+     */
+    #[ORM\ManyToMany(targetEntity: TypeGame::class, inversedBy: 'games')]
+    private Collection $Type;
+
+    /**
+     * @var Collection<int, StudioGame>
+     */
+    #[ORM\ManyToMany(targetEntity: StudioGame::class, inversedBy: 'games')]
+    private Collection $StudioGame;
+
+    public function __construct()
+    {
+        $this->Type = new ArrayCollection();
+        $this->StudioGame = new ArrayCollection();
+    }
+
     public function getId(): ?int
     {
         return $this->id;
+    }
+
+    public function getName(): ?string
+    {
+        return $this->name;
+    }
+
+    public function setName(string $name): static
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    /**
+     * @return Collection<int, TypeGame>
+     */
+    public function getType(): Collection
+    {
+        return $this->Type;
+    }
+
+    public function addType(TypeGame $type): static
+    {
+        if (!$this->Type->contains($type)) {
+            $this->Type->add($type);
+        }
+
+        return $this;
+    }
+
+    public function removeType(TypeGame $type): static
+    {
+        $this->Type->removeElement($type);
+
+        return $this;
+    }
+
+    /**
+     * @return Collection<int, StudioGame>
+     */
+    public function getStudioGame(): Collection
+    {
+        return $this->StudioGame;
+    }
+
+    public function addStudioGame(StudioGame $studioGame): static
+    {
+        if (!$this->StudioGame->contains($studioGame)) {
+            $this->StudioGame->add($studioGame);
+        }
+
+        return $this;
+    }
+
+    public function removeStudioGame(StudioGame $studioGame): static
+    {
+        $this->StudioGame->removeElement($studioGame);
+
+        return $this;
     }
 }
---
+++ src/Entity/StudioGame.php
@@ -0,0 +1,90 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\StudioGameRepository;
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\Common\Collections\Collection;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: StudioGameRepository::class)]
+class StudioGame
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $name = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $description = null;
+
+    /**
+     * @var Collection<int, Game>
+     */
+    #[ORM\ManyToMany(targetEntity: Game::class, mappedBy: 'StudioGame')]
+    private Collection $games;
+
+    public function __construct()
+    {
+        $this->games = new ArrayCollection();
+    }
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getName(): ?string
+    {
+        return $this->name;
+    }
+
+    public function setName(string $name): static
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    public function getDescription(): ?string
+    {
+        return $this->description;
+    }
+
+    public function setDescription(string $description): static
+    {
+        $this->description = $description;
+
+        return $this;
+    }
+
+    /**
+     * @return Collection<int, Game>
+     */
+    public function getGames(): Collection
+    {
+        return $this->games;
+    }
+
+    public function addGame(Game $game): static
+    {
+        if (!$this->games->contains($game)) {
+            $this->games->add($game);
+            $game->addStudioGame($this);
+        }
+
+        return $this;
+    }
+
+    public function removeGame(Game $game): static
+    {
+        if ($this->games->removeElement($game)) {
+            $game->removeStudioGame($this);
+        }
+
+        return $this;
+    }
+}
